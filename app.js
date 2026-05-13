cc6fedff0530eee6905411a8591699112ae47954
ChezScape <drewscandys@gmail.com>
2026-05-13 22:28:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,13 +7,10 @@ window.onerror = (msg, src, line) => {
         </div>
     `;
 };
-document.body.innerHTML = `
-    <div style="
-        color:white;
-        padding:40px;
-        font-family:sans-serif;
-    ">
-        <h1>BOOT TEST</h1>
-        <p>If you see this, JS is working.</p>
-    </div>
-`;
+
+document.body.innerHTML = "BOOT OK - LOADING ROUTER...";
+
+import { initRouter }
+from "./router.js";
+
+initRouter();
