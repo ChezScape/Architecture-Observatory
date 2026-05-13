9df62de466ca0df4451a444dc338699f5d1a41dc
ChezScape <drewscandys@gmail.com>
2026-05-13 23:35:18 +0100

    Update landingView.js

--- landing/landingView.js
+++ landing/landingView.js
@@ -1,27 +1,8 @@
-import { navigate } from "../router.js";
-
 export function mountLandingView(container) {
 
     container.innerHTML = `
-        <h1 style="color:white;">
-            LANDING
-        </h1>
-    `;
-}
-                    Enter Runtime
-                </button>
-
-                <button id="open-sandbox">
-                    Open Sandbox
-                </button>
-
-            </div>
+        <div style="color:white;padding:20px;">
+            <h1>LANDING VIEW</h1>
         </div>
     `;
-
-    root.querySelector("#enter-runtime")
-        .onclick = () => navigate("runtime");
-
-    root.querySelector("#open-sandbox")
-        .onclick = () => navigate("sandbox");
 }
