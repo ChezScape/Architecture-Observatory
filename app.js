8fc1f6b02a19c3bb30b8ae24f93e5cd86c9ffd99
ChezScape <drewscandys@gmail.com>
2026-05-14 06:34:09 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,10 @@
-import { navigate } from "./router.js";
-
-console.log("APP STARTED");
-
-window.addEventListener(
-    "DOMContentLoaded",
-    () => {
-
-        console.log(
-            "DOM LOADED"
-        );
-
-        const root =
-            document.getElementById(
-                "app-root"
-            );
-
-        console.log(
-            "ROOT:",
-            root
-        );
-
-        if (!root) {
-
-            document.body.innerHTML = `
-                <div style="
-                    color:white;
-                    background:black;
-                    padding:20px;
-                    font-family:sans-serif;
-                ">
-                    APP ROOT MISSING
-                </div>
-            `;
-
-            return;
-        }
-
-        navigate("landing");
-    }
-);
+document.body.innerHTML = `
+    <div style="
+        background:black;
+        color:white;
+        padding:40px;
+        font-family:sans-serif;
+    ">
+        APP.JS EXECUTED SUCCESSFULLY
+    </div>
+`;
