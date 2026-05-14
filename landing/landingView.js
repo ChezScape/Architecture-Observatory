ef0821d9af99cf7319f09fa41d5afa05025069ad
ChezScape <drewscandys@gmail.com>
2026-05-14 06:45:52 +0100

    Update landingView.js

--- landing/landingView.js
+++ landing/landingView.js
@@ -1,8 +1,49 @@
-export function mountLandingView(container) {
+export function mountLandingView(
+    root
+) {
 
-    container.innerHTML = `
-        <div style="color:white;padding:20px;">
-            <h1>LANDING VIEW</h1>
-        </div>
+    root.innerHTML = `
+        <section class="hero">
+
+            <h1 class="title">
+                Architecture Observatory
+            </h1>
+
+            <p class="subtitle">
+                Runtime intelligence,
+                architectural tracing,
+                portable sandbox analysis,
+                and live system diagnostics.
+            </p>
+
+            <div
+                style="
+                    margin-top:30px;
+                    display:flex;
+                    gap:12px;
+                    flex-wrap:wrap;
+                    justify-content:center;
+                "
+            >
+
+                <button
+                    onclick="
+                        location.hash='runtime'
+                    "
+                >
+                    Open Runtime
+                </button>
+
+                <button
+                    onclick="
+                        location.hash='sandbox'
+                    "
+                >
+                    Open Sandbox
+                </button>
+
+            </div>
+
+        </section>
     `;
 }
